ea36bd91f433cb3db8599550fe10581c955116ac
Pedro Abreu <henriqueibtou@gmail.com>
2022-09-18 23:34:43 -0700

    re submiting on heroku

--- src/components/navBarTop.js
+++ src/components/navBarTop.js
@@ -25,6 +25,7 @@ export default class NavBarTop extends Component {
                             Separated link
                             </NavDropdown.Item>
                         </NavDropdown> */}
+                        
                         </Nav>
                     </Navbar.Collapse>
                     </Container>
